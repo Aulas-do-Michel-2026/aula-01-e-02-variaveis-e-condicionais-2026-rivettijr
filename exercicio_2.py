ff797e14a0da067a5b3bf9e6c74bf77bbbfbe9c0
Anselmo Rivetti <144709330+rivettijr@users.noreply.github.com>
2026-03-28 17:02:07 -0300

    Add input for patient weight and calculate dose

--- exercicio_2.py
+++ exercicio_2.py
@@ -13,3 +13,8 @@ Digite o peso do paciente (em kg):
 Resposta:
 Média: 140 mg
 """
+
+peso = int(input('Digite o peso do paciente (Kg):'))
+dose_recomendada = 2 * peso
+
+print(f'Média: {dose_recomendada}')
